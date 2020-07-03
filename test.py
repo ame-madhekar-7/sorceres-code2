fec60c7cc81e19afb191d95d92411a88de4a24ea
ame-madhekar-7 <ame.madhekar182@gmail.com>
2020-07-03 18:09:34 +0530

    test.

---
+++ test.py
@@ -0,0 +1,14 @@
+import time
+
+time.sleep(3)
+
+name = input('What is your name? : ')
+
+s = f"hi {name}"
+print(s)
+
+time.sleep(1)
+age = input("What is your age? : ")
+
+s = f"You are {age}"
+print(s)
